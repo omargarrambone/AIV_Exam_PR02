b550baaefda1a7f5b44affd0a84a39ab40f0c25f
Omar Garrambone <omargarrambone46@gmail.com>
2023-06-05 17:40:18 +0200

    HealthBar

---
+++ Assets/Scripts/Player/HealthManager.cs
@@ -0,0 +1,35 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class HealthManager : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+
+    public HealthBarScript HealthBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+        HealthBar.SetMaxHealth(MaxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+
+        HealthBar.SetHealth(CurrentHealth);
+    }
+}
