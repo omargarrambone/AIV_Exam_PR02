c22ed967e9162bd925115e495cc0ac68a280801e
Ximer <20438890+Ximer612@users.noreply.github.com>
2023-06-07 14:39:56 +0200

    Interactor script

--- Assets/Scripts/Interactable/Interactor.cs
+++ Assets/Scripts/Interactable/Interactor.cs
@@ -4,42 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Interactor : MonoBehaviour
 {
-    [SerializeField] private LayerMask interactableLayerMask;
-    [SerializeField] private float maxDistance;
-    private RaycastHit hit;
-    [SerializeField] private Image interactImage;
-    [SerializeField] private Color defaultColor;
-    [SerializeField] private Color interactColor;
-
-    Interactable actualInteractable;
-
-    void Update()
-    {
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance, interactableLayerMask))
-        {
-            Interactable interactable = hit.collider.GetComponent<Interactable>();
-
-            if (interactable != null)
-            {
-                interactImage.color = interactColor;
-                actualInteractable = interactable;
-            }
-
-            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward*maxDistance, Color.green);
-        }
-        else
-        {
-            interactImage.color = defaultColor;
-            actualInteractable = null;
-            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward*maxDistance, Color.red);
-        }
-
-    }
+    [SerializeField] private Interactable actualInteractable;
+    [SerializeField] private TMPro.TMP_Text guiText;
 
     public void Interact(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
-        if(context.performed)
-            actualInteractable.onInteract.Invoke();
+        if (context.performed)
+        {
+            if(actualInteractable is not null)
+            {
+                actualInteractable.OnInteract.Invoke();
+                guiText.SetText("");
+            }
+        }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        actualInteractable = other.GetComponent<Interactable>();
+        guiText.SetText($"Take [{actualInteractable.ItemName}]");
+        //fai comparire scritta premi E per interagire ed altro
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        actualInteractable = null;
+        guiText.SetText("");
+    }
 }
