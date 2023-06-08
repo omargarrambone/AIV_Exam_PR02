2d5b5c4339a1d634c62205872cc9b6a379c74bb3
iumex <robertomessina7@gmail.com>
2023-06-08 11:36:58 +0200

    Implementation Field Of View

--- Assets/Scripts/EnemyAi/BasicEnemyAgentAi.cs
+++ Assets/Scripts/EnemyAi/BasicEnemyAgentAi.cs
@@ -6,39 +6,41 @@ using UnityEngine.AI;
 public class BasicEnemyAgentAi : MonoBehaviour
 {
     public NavMeshAgent agent;
+    public float patrolSpeed = 3;
+    public float chaseSpeed = 6;
     public List<Transform> patrolWaypoints;
     public Transform playerTarget;
-    public float activationDistance = 5f;
+    //public float activationDistance = 5f;
     public int currentWaypoint;
+    public FieldOfView fov;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        fov = GetComponent<FieldOfView>();
         SetNewWaypoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distanceFromTarget = Vector3.Distance(playerTarget.position, agent.transform.position);
-        if (distanceFromTarget <= activationDistance)
+        //float distanceFromTarget = Vector3.Distance(playerTarget.position, agent.transform.position);
+        //if (fov.targetCheck() == true && distanceFromTarget <= activationDistance)
+        
+        if (fov.targetCheck() == true)
         {
+            agent.speed = chaseSpeed;
             agent.SetDestination(playerTarget.position);
         }
         else if (agent.remainingDistance < 0.5f)
         {
+            agent.speed = patrolSpeed;
             SetNewWaypoint();
         }
     }
 
-    private void FixedUpdate()
-    {
-        //if (agent.remainingDistance < 0.5f)
-        //{
-        //    SetNewWaypoint();
-        //}
-    }
 
     public void SetNewWaypoint()
     {
---
+++ Assets/Scripts/EnemyAi/FieldOfView.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float radius;
+
+    public float angle = 110f;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    public bool canSeePlayer;
+
+    private float timerCheck = 0.5f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        timerCheck -= Time.deltaTime;
+        if (timerCheck <= 0)
+        {
+            targetCheck();
+            timerCheck = 0.5f;
+        }
+    }
+
+
+    public bool targetCheck()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (hitColliders.Length != 0)
+        {
+            Transform target = hitColliders[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle * 0.5f)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask))
+                {
+                    canSeePlayer = true;
+                    //Debug.Log("I see You!");
+                    return canSeePlayer;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+        return canSeePlayer;
+    }
+}
