f615c4b4adde9ea9112fdb2994c5f2706eda9c7e
iumex <robertomessina7@gmail.com>
2023-06-13 19:22:23 +0200

    small correction

--- Assets/Scripts/Enemy/StunnManager.cs
+++ Assets/Scripts/Enemy/StunnManager.cs
@@ -41,7 +41,7 @@ public class StunnManager : MonoBehaviour
 
         if (CurrentStunn >= 100)
         {
-s            OnStun.Invoke();
+            OnStun.Invoke();
             IsStunned = true;
         }
     }
