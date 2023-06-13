5766c66705d98c09933547fd274a83f5ed04778c
Ximer <20438890+Ximer612@users.noreply.github.com>
2023-06-13 13:02:26 +0200

    Fixed arancini and player stuff

---
+++ Assets/Scripts/Flute/AranciniRotation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AranciniRotation : MonoBehaviour
+{
+    public static Vector3 rotationVector = new Vector3(0f,0f,90f);
+    void Update()
+    {
+        transform.Rotate(rotationVector * Time.deltaTime);
+    }
+}
