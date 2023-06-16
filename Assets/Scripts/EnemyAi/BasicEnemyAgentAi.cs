bf9e92f43efde94c0bcd1fa0b37008464f691dbe
iumex <robertomessina7@gmail.com>
2023-06-16 19:15:09 +0200

    State Dead

--- Assets/Scripts/EnemyAi/BasicEnemyAgentAi.cs
+++ Assets/Scripts/EnemyAi/BasicEnemyAgentAi.cs
@@ -136,13 +136,17 @@ public class BasicEnemyAgentAi : MonoBehaviour
                     Ucelletti.gameObject.SetActive(false);                   
                     StunnManager.IsStunned = false;
                 }
-                else if (HealthManager.CurrentHealth <= 0)
-                {
-                    Anim.SetBool("Stunned", false);
-                    Ucelletti.gameObject.SetActive(false);
-                    StunnManager.IsStunned = false;
-                    Agent.speed = 0;
-                }
+                //else if (HealthManager.CurrentHealth <= 0)
+                //{
+                //    Anim.SetBool("Stunned", false);
+                //    Ucelletti.gameObject.SetActive(false);
+                //    StunnManager.IsStunned = false;
+                //    Agent.speed = 0;
+                //}
+                break;
+
+            case EnemyState.Dead:
+                Agent.GetComponent<BasicEnemyAgentAi>().enabled = false;
                 break;
             default:
                 break;
