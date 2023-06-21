031ac5b27eb07afd7f3097e970f4533c4737d090
Nicolò Centonze <nico.101.ce@gmail.com>
2023-06-21 20:59:34 +0200

    SwordSlashFx on point

--- Assets/PlayerAttackScript.cs
+++ Assets/PlayerAttackScript.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.VFX;
 
 public class PlayerAttackScript : MonoBehaviour
 {
-    public UnityEvent OnStartAttack,OnEndAttack;
+    public UnityEvent OnStartAttack, OnEndAttack;
+    public VisualEffect swordSlash;
+
 
     public void CallOnStartHit()
     {
-        InventoryManager.SetObjectsColliders(true);
 
-        OnStartAttack.Invoke();
+
+        if (InventoryManager.CurrentSlotIndex == 0)
+        {
+            InventoryManager.SetObjectsColliders(true);
+            OnStartAttack.Invoke();
+
+        }
+        else
+        {
+            InventoryManager.SetObjectsColliders(true);
+            OnStartAttack.Invoke();
+
+            swordSlash.playRate = 0.80f;
+            swordSlash.Play();
+
+        }
     }
 
     public void CallOnEndHit()
     {
         InventoryManager.SetObjectsColliders(false);
 
+        swordSlash.Stop();
         OnEndAttack.Invoke();
+
     }
 }
