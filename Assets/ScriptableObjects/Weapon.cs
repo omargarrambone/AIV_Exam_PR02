5127fc9b5d2babba936e291c105c1f0c90257e6b
Ximer <20438890+Ximer612@users.noreply.github.com>
2023-06-22 17:32:23 +0200

    Scriptable objects

--- Assets/ScriptableObjects/Weapon.cs
+++ Assets/ScriptableObjects/Weapon.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[CreateAssetMenu(fileName = "New Weapon" , menuName = "CustomMenu / Weapon")]
 public class Weapon : ScriptableObject
 {
     public string Name;
     public float BloodDamage;
     public float StunDamage;
+    public Sprite DisplayImage;
+
 }
