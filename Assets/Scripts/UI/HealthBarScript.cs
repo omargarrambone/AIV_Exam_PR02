31930524108592d4b674ef732791421bcc9a5324
Ximer <20438890+Ximer612@users.noreply.github.com>
2023-06-25 18:19:16 +0200

    Fixed enemy ui

--- Assets/Scripts/UI/HealthBarScript.cs
+++ Assets/Scripts/UI/HealthBarScript.cs
@@ -6,37 +6,34 @@ using UnityEngine.UI;
 
 public class HealthBarScript : MonoBehaviour
 {
-    public Slider slider;
-
     public Gradient gradient;
-
     public Image fill;
+    private float maxHealth;
+    private float maxStun;
 
     public virtual void SetMaxHealth(float health)
     {
-        slider.maxValue = health;
-        slider.value = health;
-
+        maxHealth = health;
+        fill.fillAmount = maxHealth;
         fill.color = gradient.Evaluate(1f);
     }
 
-    public void SetMinStunnValue(float stunn)
+    public void SetMinStunnValue(float stun)
     {
-        slider.minValue = stunn;
-        slider.value = stunn;
-
+        fill.fillAmount = stun;
+        maxStun = 100;
         fill.color = gradient.Evaluate(1f);
     }
 
     public virtual void SetHealth(float health)
     {
-        slider.value = health;
-        fill.color = gradient.Evaluate(slider.normalizedValue);
+        fill.fillAmount = health/maxHealth;
+        fill.color = gradient.Evaluate(health / maxHealth);
     }
 
     public void SetStunn(float value)
     {
-        slider.value = value;
-        fill.color = gradient.Evaluate(value);
+        fill.fillAmount = value/maxStun;
+        fill.color = gradient.Evaluate(value / maxStun);
     }
 }
