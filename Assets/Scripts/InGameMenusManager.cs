e83a97ee957faaaa279df7b9d9fef6d3f1dc665e
Ximer <20438890+Ximer612@users.noreply.github.com>
2023-06-27 13:10:37 +0200

    Fixed campfire menu

---
+++ Assets/Scripts/InGameMenusManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenusManager : MonoBehaviour
+{
+    public static GameObject PauseMenu, SaveMenu;
+    [SerializeField] private GameObject pauseMenu, saveMenu;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PauseMenu = pauseMenu;
+        SaveMenu = saveMenu;
+    }
+
+    public static void ShowHidePauseMenu(bool value)
+    {
+        PauseMenu.SetActive(value);
+    }
+
+    public static void ShowHidePauseMenu()
+    {
+        PauseMenu.SetActive(!PauseMenu.activeSelf);
+    }
+
+    public static void ShowHideSaveMenu(bool value)
+    {
+        SaveMenu.SetActive(value);
+    }
+
+    public static void ShowHideSaveMenu()
+    {
+        SaveMenu.SetActive(!SaveMenu.activeSelf);
+    }
+}
